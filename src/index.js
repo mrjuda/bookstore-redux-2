348c812938a59031c084edf152716b9c60f162dc
judah <87juda@gmail.com>
2022-07-23 17:38:54 -0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
+// index.js
+
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/configureStore';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import App from './App';
+import { store } from './redux/configureStore';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
